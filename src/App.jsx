2081af07afc7c8861d2cb74bb1262ea2c8ef7ba2
Capta1nHamster <100691740+SeleverViktor@users.noreply.github.com>
2025-02-17 11:29:12 +0100

    szükséges modulok imort
    bootstrap,router,tachyons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
-import { useState } from 'react'
+import  React from "react"
+import Home from "./oldalak/Home"
+import Mermesek from "./Mermesek"
+import Ermesek from "./Ermesek"
+import Navbar from "./Navbar"
 
+import {Route,Routes} from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'tachyons'
 
 
 function App() {
---
+++ src/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+const Navbar=(()=>{
+
+    return(
+        <div>
+            <h1>
+                Menu
+            </h1>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero sint fugit labore blanditiis corporis non deleniti enim velit modi veritatis magnam saepe unde atque nisi fugiat, provident ex quibusdam ab porro. Reiciendis fuga ratione facilis veniam sit! Minima sed magni autem nulla quis delectus omnis enim officiis! Deleniti illo quasi velit iste voluptatibus soluta qui recusandae, saepe incidunt nulla debitis mollitia non sit harum, suscipit nisi, quod molestias tempore beatae? Qui sunt quos porro, sapiente non, quaerat in itaque dolore deserunt omnis assumenda facere magni enim error reprehenderit neque, ullam optio aliquid doloribus libero pariatur nobis! Aspernatur molestiae numquam ab?
+            </p>
+        </div>
+    )
+})
+export default Navbar
