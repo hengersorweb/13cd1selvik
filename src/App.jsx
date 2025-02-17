5e1983ef8dc89220f9ba3384baa985ee8dc78cb5
Capta1nHamster <100691740+SeleverViktor@users.noreply.github.com>
2025-02-17 11:06:31 +0100

    alaprendszer beallitasa
    A reactbootstrap ,bootstrap , router és a tachyos module telepítése .
    Töröltük a szükségtelen állományokat ,
    összes css, main.jsx tartalma csak a kötelező maradt és a app.jsx is csak ugyanezt tartalmazza.

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>covid</h1>
+      </div>
+        
+          
+    </>
+  )
+}
+
+export default App
